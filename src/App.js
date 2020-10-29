b906ad8f8773a4a11ea78ff4f7c12fcda1410205
Jesse Quijano <jesse@quijano.net>
2020-10-28 17:48:25 -0700

    create input box so user can enter search term

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import React, { useState, useEffect } from 'react';
 const App = () => {
     //state
     const [news, setNews] = useState([]);
+    const [searchQuery, setSearchQuery] = useState('react');
 
     // fetch news
     const fetchNews = () => {
-        fetch('http://hn.algolia.com/api/v1/search?query=react')
+        fetch(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`)
             .then(result => result.json())
             .then(data => setNews(data.hits))
             .catch(error => console.log(error));
@@ -14,11 +15,20 @@ const App = () => {
 
     useEffect(() => {
         fetchNews();
-    });
+    }, [searchQuery]);
+
+    const handleChange = (e) => {
+        console.log(e.target.value);
+        setSearchQuery(e.target.value);
+    };
 
     return (
         <div>
             <h2>News</h2>
+            <form>
+                <input type="text" value={searchQuery} onChange={handleChange}></input>
+                <button>Search</button>
+            </form>
             {news.map((n, i) => {
                 return <p key={i}>{n.title}</p>
             })}
